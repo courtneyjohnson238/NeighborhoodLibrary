33710984651a18a9d80d37e2d0f3106fba74a7c9
courtneyjohnson238 <courtneyjohnson238@gmail.com>
2023-04-21 15:37:34 -0400

    added new files

---
+++ src/main/java/NeighborhoodLibrary.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NeighborhoodLibrary {
+
+    private static Book[] books = new Book[5];
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Book book1 = new Book(123,"21345", "New Book");
+        Book book2 = new Book(456,"21144", "Rich Dad Poor Dad");
+        Book book3 = new Book(789, "54321", "The Secret");
+        Book book4 = new Book(101, "34567", "Eve in Exile");
+        Book book5 = new Book(345, "89103","Mere Christianity");
+
+
+
+    }
+}
